a7628e8a7ca81cc84aa38735e162f41156b30448
kaser.lau <kaser.lau@outlook.com>
2018-10-21 11:27:25 +0800

    commit springboot project

---
+++ src/main/java/com/better/knowledgepooling/KnowledgepoolingApplication.java
@@ -0,0 +1,12 @@
+package com.better.knowledgepooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgepoolingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgepoolingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/better/knowledgepooling/KnowledgepoolingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.better.knowledgepooling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnowledgepoolingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
