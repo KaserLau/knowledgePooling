b27842fa9c928681ef8c0efb3a6235435559b22a
yaobin.a.liu <yaobin.a.liu@accenture.com>
2019-02-24 17:33:28 +0800

    Xml To Java bean Pojo

--- src/main/java/com/better/knowledgepooling/controller/XmlToJavaPojoController.java
+++ src/main/java/com/better/knowledgepooling/controller/XmlToJavaPojoController.java
@@ -28,7 +28,7 @@ public class XmlToJavaPojoController {
     * 2.这里POJO bean BaggageAllowanceResponse 在使用 Unmarshaller 的使用需要用到指定的注解来启动对XML中字符串的识别：
     *   a. 如这里xml数据中的<baggageResponse>是开头的标签，需要用到@XmlRootElement注解指定该标签的内容使它对应 Java Bean BaggageAllowanceResponse，完整的的使用：@XmlRootElement(name = "baggageResponse")
     *   b. 使用@XmlElement(name = "document", required = true)，指定XML 数据 <document>的内容，对应的就是Document对象：private Document document
-    *   c. 当XML中有多个相同标签内容的时候其实对应的就是Java中ArrayList的对象： 如：<originDestination>***</originDestination><originDestination>***</originDestination> 对应的就是：private List<OriginDestination> originDestination;
+    *   c. 当XML中有多个相同标签内容的时候其实对应的就是Java中ArrayList的对象： 如：<originDestination>***</originDestination><originDestination>***</originDestination> 对应的就是：private List<OriginDestination> originDestination
     *   d. 如XML中出现： <mediaList/>这种类型的数据时，表明mediaList是没东西的，但是对应的是private MediaList mediaList;也会接收但是值为null
     * 3.需要注意的是使用标签@XmlElement(name = "document")或者@XmlRootElement 里面的名字一定要与XML标签指定的值大小写一致。
     * 4.注意：使用注解@XmlElement的前提需要在bean class中加入注解@XmlAccessorType(XmlAccessType.NONE)，否则@XmlElement会无效并且报错。
